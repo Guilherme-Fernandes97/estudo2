5e4345de8013f1165bd6ccf1151cf05e40038bf8
Guilherme <guilherme_fernandes1997@hotmail.com>
2022-07-20 22:48:33 -0300

    Exercicio finalizado

---
+++ exercicios_for.py
@@ -0,0 +1,15 @@
+vendas = [100, 150, 1500, 2000, 120]
+meta = 110
+
+for venda in vendas:
+    if venda < meta:
+        print('A loja não ganha bônus')
+        break
+
+meta = 130
+vendedores = ['João', 'Julia', 'Ana', 'José', 'Maria']
+
+for venda in vendas:
+    if venda < meta:
+        continue
+    print(venda)
