71408145ff36411f7a6b5db90cc9014142404d2e
Guilherme <guilherme_fernandes1997@hotmail.com>
2022-07-21 14:08:59 -0300

    exercicio finalizado

--- exercicios_while/exercicio_while1.py
+++ exercicios_while/exercicio_while1.py
@@ -1,1 +1,16 @@
-print('teste2')
+vendas = [
+    ['maçã', 5],
+    ['banana', 15],
+    ['azeite', 1],
+    ['vinho', 3],
+]
+
+vendas = []
+
+while True:
+    produto = input('Qual o produto ?')
+    if not produto:
+        break
+    qtde = int(input('qual a quantidade ?'))
+    vendas.append([produto, qtde])
+print(vendas)
