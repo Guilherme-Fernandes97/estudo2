3f050c9f297c99c4e596527a1b272e7ab5816d14
Guilherme <guilherme_fernandes1997@hotmail.com>
2022-08-10 18:22:17 -0300

    exercicio finalizado

---
+++ exercicios_function/exercicio_function13.py
@@ -0,0 +1,19 @@
+def minha_soma(*numeros):
+    soma = 0
+    for numero in numeros:
+        soma += numero
+    return soma
+
+print(minha_soma(10, 20, 30, 40))
+
+def preco_final(preco, **adicionais):
+    #print(adicionais)
+    if 'desconto' in adicionais:
+        preco *= (1 - adicionais['desconto'])
+    if 'garantia_extra' in adicionais:
+        preco += adicionais['garantia_extra']
+    if 'imposto' in adicionais:
+        preco *= (1 + adicionais['imposto'])
+    return preco
+
+print(preco_final(1000, desconto=0.1, garantia_extra=100))
