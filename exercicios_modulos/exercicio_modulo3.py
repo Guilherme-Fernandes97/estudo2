b605a3a542a6d96705df3a64a407857e16af1ecf
Guilherme <guilherme_fernandes1997@hotmail.com>
2022-08-11 20:42:36 -0300

    exercicio finalizado

---
+++ exercicios_modulos/exercicio_modulo3.py
@@ -0,0 +1,10 @@
+import keyboard
+import webbrowser
+
+webbrowser.open('https://www.google.com')
+
+keyboard.press_and_release('ctrl+t')
+
+keyboard.write('hashtagtreinamentos.com')
+
+keyboard.press_and_release('enter')
